0392ae46ebb60b3371a12e0a806234f8daf7905f
mabulgu <mabulgu@gmail.com>
2026-08-05 01:47:50 +0300

    Rewrite formula with proper depends_on and virtualenv_install_with_resources

--- strimzi-kafka-cli.rb
+++ strimzi-kafka-cli.rb
@@ -2,31 +2,19 @@ class StrimziKafkaCli < Formula
   include Language::Python::Virtualenv
 
   desc "Command Line Interface for Strimzi Kafka Operator"
-  homepage "https://github.com/systemcraftsman/strimzi-kafka-cli"
-  version "1.0.0b1"
-  url "https://files.pythonhosted.org/packages/source/s/strimzi-kafka-cli/strimzi-kafka-cli-"+version+".tar.gz"
+  homepage "https://strimzi-cli.dev"
+  url "https://files.pythonhosted.org/packages/cc/63/30792d11113764fa34bcd439cbed47adf5b5e704e9a60c36454afd481bb5/strimzi_kafka_cli-1.0.0b1.tar.gz"
+  sha256 "48fc9332be1e4e6234941c9fd81f2aa3b6429fdccde20bba2b6367bb7fc3f8db"
   license "Apache-2.0"
 
-  depends_on ["python@3.13", "python@3.12", "python@3.11", "python@3.10", "python@3.9"]
+  depends_on "python@3.12"
 
   def install
-    venv = virtualenv_create(libexec, python="python3.12", without_pip: false)
-
-    @venv_root = venv.instance_variable_get(:@venv_root)
-    @formula = venv.instance_variable_get(:@formula)
-
-    bin_before = Dir[@venv_root/"bin/*"].to_set
-
-    system @venv_root/"bin/pip", "install", "strimzi-kafka-cli==" + version
-
-    bin_after = Dir[@venv_root/"bin/*"].to_set
-    bin_to_link = (bin_after - bin_before).to_a
-
-    @formula.bin.install_symlink(bin_to_link)
+    virtualenv_install_with_resources
   end
 
   test do
     assert_match "Strimzi Kafka CLI", shell_output("#{bin}/kfk")
-    assert_match "CLI Version: " + version, shell_output("#{bin}/kfk --version")
+    assert_match "CLI Version:", shell_output("#{bin}/kfk --version")
   end
 end
